2cd0912b45aa99b1f75bff50aac750a568e914f4
JTeixeiraz <joaopedroteixeirareis@gmail.com>
2025-03-08 10:33:01 -0300

    Adicionando a funcao de alterar estoque, exibir estoque e calcular preco de produto expecifico

--- bin/gestaodeprodutosdart.dart
+++ bin/gestaodeprodutosdart.dart
@@ -9,8 +9,119 @@ void main(List<String> arguments) {
 }
 
 
-class Produtos {
+class Empresa {
   List<Map<String, dynamic>> produtos = [];
+
+  void adicionarProduto() {
+    Produtos produto = Produtos();
+    print('Qual o nome do produto que deve ser adicionado ao estoque?:');
+    String nomeProd = stdin.readLineSync() ?? "";
+    produto.setNome = nomeProd.toUpperCase();
+    print('Informe a quantidade disponivel do produto:');
+    String quantiProd = stdin.readLineSync() ?? "";
+    produto.setQuantidade = int.parse(quantiProd);
+    print('Informe o preço do produto que será adicionado:');
+    String precoProd = stdin.readLineSync() ?? "";
+    produto.setPreco = double.parse(precoProd);
+    produtos.add({"nome":produto.getNome, "quantidade":produto.getQuantidade, "preco":produto.getPreco, "id":produtos.length});
+    print('Produto adicionado + id unico implementado');
+    print(produtos[produtos.length]);
+    print('');
+    print('Exibir o estoque? \n (y/n)');
+    String escolhaexibir = stdin.readLineSync()??"";
+    switch (escolhaexibir) {
+      case "y":
+        exibirProdutos();
+        break;
+      case "n":
+      print('');
+        break;
+      default:
+      print('Digite y para sim ou n para nao');
+      break;
+    }
+}
+
+  void removerProduto(){
+    print('Adicione o id do produto que deseja remover(caso não saiba, vá na opção de listar produtos e procure o produto que deseja remover, lá estará o id):');
+    String idProduto = stdin.readLineSync()??"";
+    int produtoRemovido = int.parse(idProduto);
+    try {
+      print('O produto: ${produtos[produtoRemovido]["nome"]} foi removido com sucesso.');
+      produtos.removeAt(produtoRemovido);
+    } catch (e) {
+      print('Erro: $e');
+      print('Provavelmente o produto que tentou remover não existe.');
+    }
+  }
+
+  void exibirProdutos(){
+    print("============== Estoque ==============");
+    for (var i = 0; i < produtos.length; i++) {
+      print(produtos[i]);
+    }
+  }
+
+  void calcularProdutosPreco(){
+    print('Digite o ID do produto que deseja calcular:');
+    String idProdutoCalcular = stdin.readLineSync()??"";
+    int? valorIdProdutoCalcular = int.tryParse(idProdutoCalcular)??0;
+    print('Nome do produto: \n ${produtos[valorIdProdutoCalcular]["nome"]}');
+    print('Quantidade em estoque: \n ${produtos[valorIdProdutoCalcular]["quantidade"]}');
+    print('Preco do produto: zn ${produtos[valorIdProdutoCalcular]["preco"]}');
+    print('=========================');
+    double calculo = produtos[valorIdProdutoCalcular]["preco"]*produtos[valorIdProdutoCalcular]["quantidade"];
+    print('Valor total de todos os produtos em estoque somados: \n $calculo');
+  }
+
+  void atualizarEstoque(){
+    bool sairAtualizarEstoque = false;
+    while(!sairAtualizarEstoque){
+      print('Oque deseja alterar em um produto?');
+      print('1-Quantidade');
+      print('2-Preço');
+      print('3-Sair');
+      String escolha = stdin.readLineSync()??"";
+      switch (escolha) {
+        case "1":
+            mudarQuantidade();
+          break;
+        case "2":
+            mudarPreco();
+          break;
+        case "3":
+        sairAtualizarEstoque = true;
+          break;
+        default:
+      }
+    }
+  }
+
+  void mudarQuantidade(){
+    print('Digite o Id do produto que deseja alterar:');
+    String escolhaProduto = stdin.readLineSync()??"";
+    int? valEscolhaProduto = int.tryParse(escolhaProduto) ??0;
+    print('Qual a quantidade que deseja alterar no estoque?:');
+    String txtQuantidade = stdin.readLineSync()??"";
+    int? valorQuantidade = int.tryParse(txtQuantidade) ??0;
+    produtos[valEscolhaProduto]["quantidade"] = valorQuantidade;
+    print('Valor alterado com sucesso');
+  }
+
+  void mudarPreco(){
+    print('Digite o Id do produto que deseja alterar:');
+    String escolhaProduto = stdin.readLineSync()??"";
+    int? valEscolhaProduto = int.tryParse(escolhaProduto) ??0;
+    print('Qual sera o novo preço do ${produtos[valEscolhaProduto]["nome"]}:');
+    String txtPreco = stdin.readLineSync()??"";
+    int? valorQuantidade = int.tryParse(txtPreco) ??0;
+    produtos[valEscolhaProduto]["preco"] = valorQuantidade;
+    print('Valor alterado com sucesso');
+  }
+}
+
+
+class Produtos{
   late String _nome;
   late int _quantidade;
   late double _preco;
@@ -28,70 +139,5 @@ class Produtos {
   get getPreco => _preco;
   set setPreco(double preco){
     _preco = preco;
-  }
-
-  void adicionarProduto() {
-    print('Qual o nome do produto que deve ser adicionado ao estoque?:');
-    String nomeProd = stdin.readLineSync() ?? "";
-    setNome = nomeProd.toUpperCase();
-    print('Informe a quantidade disponivel do produto:');
-    String quantiProd = stdin.readLineSync() ?? "";
-    setQuantidade = int.parse(quantiProd);
-    print('Informe o preço do produto que será adicionado:');
-    String precoProd = stdin.readLineSync() ?? "";
-    setPreco = double.parse(precoProd);
-    produtos.add({"nome":getNome, "quantidade":getQuantidade, "preco":getPreco, "id":produtos.length});
-    print('Produto adicionado + id unico implementado');
-    print(produtos[produtos.length]);
-  }
-
-  void removerProduto(){
-    print('Adicione o id do produto que deseja remover(caso não saiba, vá na opção de listar produtos e procure o produto que deseja remover, lá estará o id):');
-    String idProduto = stdin.readLineSync()??"";
-    int produtoRemovido = int.parse(idProduto);
-    try {
-      print('O produto: ${produtos[produtoRemovido]["nome"]} foi removido com sucesso.');
-      produtos.removeAt(produtoRemovido);
-    } catch (e) {
-      print('Erro: $e');
-      print('Provavelmente o produto que tentou remover não existe.');
-    }
-  }
-
-  void atualizarEstoque(){
-    bool sair = false;
-    while(!sair){
-      print('Oque deseja alterar em um produto?');
-      print('1-Quantidade');
-      print('2-Preço');
-      print('5-Sair');
-      String escolha = stdin.readLineSync()??"";
-      switch (escolha) {
-        case "1":
-
-          break;
-        default:
-      }
-    }
-  }
-
-  void mudarQuantidade(){
-    print('De qual forma deseja encontrar o produto expecifico para altera-lo? \n 1-ID \n 2-Nome');
-    String escolha = stdin.readLineSync()??"";
-    switch (escolha) {
-      case "1":
-        print('Digite o id do produto que deseja alterar');
-        String idproduto = stdin.readLineSync()??"";
-        int valorDoId = int.parse(idproduto);
-        print("Qual a nova quantidade do produto?:");
-        String usuarioQuantidade = stdin.readLineSync()??"";
-        int valorQuantidade = int.parse(usuarioQuantidade);
-        produtos[valorDoId]["quantidade"]=valorQuantidade;
-        break;
-      case "2":
-          print('Digite o nome do produto que deseja alterar');
-        break;
-      default:
-    }
   }
 }
